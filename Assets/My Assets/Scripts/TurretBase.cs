3e4f797be0b7844f8e0a678e3f5c0f247ed76833
PeteJBB <peter.blunden@gmail.com>
2014-05-19 22:53:45 +0100

    TurretBase

--- Assets/My Assets/Scripts/TurretBase.cs
+++ Assets/My Assets/Scripts/TurretBase.cs
@@ -1,15 +1,153 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TurretBase : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class TurretBase : MonoBehaviour 
+{
+	[SerializeField]
+	protected float DetectionRange = 100;
 	
+	[SerializeField]
+	protected float FiringRange = 80;
+	
+	[SerializeField]
+	protected float ReloadTime = 2;
+	
+	[SerializeField]
+	protected float LockTime = 2;
+	
+	[SerializeField]
+	protected float AttentionSpan = 2; // how long after losing sight will turret begin searching again
+	
+	[SerializeField]
+	protected float LockSize = 10;
+	
+	[SerializeField]
+	protected float TurretSpeed = 200;
+	
+	[SerializeField]
+	protected float MaxHealth = 10;
+	
+	[SerializeField] 
+	protected GameObject DeathExplosion;
+	
+	[SerializeField]
+	protected GameObject Projectile;
+	
+	protected GameObject _target;
+	protected Transform _platform;
+	protected Transform _launcher;
+	
+	protected Vector3 _aimPoint;
+	protected float _lastSeenTime;
+	protected float _lockResetTime;
+	protected float _lastShootTime;
+	
+	protected GameObject _hud;
+	
+	protected float _health;
+	
+	// Use this for initialization
+	protected void Start () 
+	{
+		Debug.Log("Base start");
+
+		_health = MaxHealth;
+		
+		_target = GameObject.FindGameObjectWithTag("Player");
+		_platform = transform.FindChild("platform");
+		_launcher = _platform.FindChild("launcher");
+		
+		_hud = GameObject.FindGameObjectWithTag("HUD");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected void Update () 
+	{
+		Debug.Log("Base Update");
+		var vect = _target.transform.position - _launcher.position; 
+		if(vect.magnitude <= DetectionRange 
+		   && Utility.CanObjectSeeAnother(_launcher.gameObject, _target))
+		{
+			// can see target  
+			_hud.SendMessage("Detected", gameObject, SendMessageOptions.DontRequireReceiver);
+			_lastSeenTime = Time.fixedTime;
+			
+			// compute lead
+			_aimPoint = CalculateAimPoint();
+			RotateTowards(_aimPoint);
+			
+			var aimVect = _aimPoint - _launcher.position;
+			var angle = Vector3.Angle(_launcher.forward, aimVect);
+			if(angle <= LockSize && Utility.CanSeePoint(_launcher.transform.position, _aimPoint, _target))
+			{
+				Debug.DrawLine(_launcher.position, _aimPoint, Color.green);
+				if(aimVect.magnitude <= FiringRange // in range
+				   && Time.fixedTime - _lockResetTime >= LockTime // locked
+				   && Time.fixedTime - _lastShootTime >= ReloadTime) // loaded
+				{
+					Fire();
+				}
+			}
+			else
+			{
+				Debug.DrawLine(_launcher.position, _aimPoint, Color.red);
+				_lockResetTime = Time.fixedTime;
+			}
+		}
+		else
+		{
+			if(Time.fixedTime - _lastSeenTime > AttentionSpan)
+			{
+				// pick a new random aim point and wander
+				_aimPoint = _launcher.transform.position + new Vector3(Random.Range(-500, 500), Random.Range(-50, 50), Random.Range(-500, 500));
+				_lastSeenTime = Time.fixedTime;
+			}
+			RotateTowards(_aimPoint);
+		}
+	}
+
+	protected virtual Vector3 CalculateAimPoint()
+	{
+		var vect = _target.transform.position - _launcher.position;
+		var rocketTimeToTarget = vect.magnitude / 100;
+		var aim = _target.transform.position + (_target.rigidbody.velocity * rocketTimeToTarget);
+		return aim;
+	}
 	
+	protected void RotateTowards(Vector3 point)
+	{
+		var vect = point - _launcher.position;
+		
+		// rotate platform (y-axis only)
+		var v = new Vector3(vect.x, 0, vect.z);
+		var desiredRotation = Quaternion.LookRotation(v);
+		_platform.rotation = Quaternion.RotateTowards(_platform.rotation, desiredRotation, TurretSpeed * Time.deltaTime);
+		
+		// rotate lancher - (x-axis only)
+		v = new Vector3(0, vect.y, v.magnitude);
+		desiredRotation = Quaternion.LookRotation(v);
+		_launcher.localRotation = Quaternion.RotateTowards(_launcher.localRotation, desiredRotation, TurretSpeed * Time.deltaTime);
+	}
+	
+	protected void Fire()
+	{
+		_lastShootTime = Time.fixedTime;
+		
+		var pos = _launcher.position;
+		var clone = (GameObject)Instantiate(Projectile, pos, transform.rotation);
+		clone.SendMessage("SetParent", gameObject, SendMessageOptions.DontRequireReceiver);
+		Physics.IgnoreCollision(collider, clone.collider);
+		clone.transform.rotation = _launcher.rotation;
+		clone.rigidbody.AddForce(clone.transform.forward * 100, ForceMode.Impulse);
+	}
+	
+	protected virtual void ApplyDamage(float amount)
+	{
+		_health -= amount;
+		if(_health <= 0) 
+		{
+			Instantiate(DeathExplosion, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
 	}
 }
--- Assets/My Assets/Scripts/TurretPlasma.cs
+++ Assets/My Assets/Scripts/TurretPlasma.cs
@@ -1,152 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TurretPlasma : MonoBehaviour 
+public class TurretPlasma : TurretBase 
 {
-    [SerializeField]
-    float DetectionRange = 100;
-    
-    [SerializeField]
-    float FiringRange = 80;
-    
-    [SerializeField]
-    float ReloadTime = 2;
-    
-    [SerializeField]
-    float LockTime = 2;
-    
-    [SerializeField]
-    float AttentionSpan = 2; // how long after losing sight will turret begin searching again
-    
-    [SerializeField]
-    float LockSize = 10;
-    
-    [SerializeField]
-    float TurretSpeed = 200;
-    
-    [SerializeField]
-    float MaxHealth = 10;
-    
-    [SerializeField] 
-    GameObject DeathExplosion;
-    
-    [SerializeField]
-    GameObject Projectile;
-    
-    GameObject _target;
-    Transform _platform;
-    Transform _launcher;
-	
-    Vector3 _aimPoint;
-	bool _canSeeTarget;
-    float _lastSeenTime;
-    float _lockResetTime;
-    float _lastShootTime;
-    
-	GameObject _hud;
-
-    float _health;
-    
     // Use this for initialization
     void Start () 
     {
-        _health = MaxHealth;
-        
-        _target = GameObject.FindGameObjectWithTag("Player");
-        _platform = transform.FindChild("platform");
-        _launcher = _platform.FindChild("launcher");
-
-		_hud = GameObject.FindGameObjectWithTag("HUD");
+		Debug.Log("Plasma start");
+		base.Start();
     }
     
-    // Update is called once per frame
-    void Update () 
-    {
-        var vect = _target.transform.position - _launcher.position; 
-        if(vect.magnitude <= DetectionRange 
-           && Utility.CanObjectSeeAnother(_launcher.gameObject, _target))
-        {
-            // can see target  
-			_hud.SendMessage("Detected", gameObject, SendMessageOptions.DontRequireReceiver);
+	protected override Vector3 CalculateAimPoint()
+	{
+		var vect = _target.transform.position - _launcher.position; 
 
-			_canSeeTarget = true;
-            _lastSeenTime = Time.fixedTime;
-            
-            // compute lead
-            var rocketTimeToTarget = vect.magnitude / 75;
-            _aimPoint = _target.transform.position + (_target.rigidbody.velocity * rocketTimeToTarget);
+		// compute lead
+		var rocketTimeToTarget = vect.magnitude / 75;
+		var aim = _target.transform.position + (_target.rigidbody.velocity * rocketTimeToTarget);
+		
+		// aim above target for lobbing
+		var dist = new Vector3(vect.x, 0, vect.z).magnitude;
+		aim += Vector3.up * dist * dist / 1000;
 
-			// aim above target for lobbing
-			var dist = new Vector3(vect.x, 0, vect.z).magnitude;
-			_aimPoint += Vector3.up * dist * dist / 1000;
-            RotateTowards(_aimPoint);
-            
-            var aimVect = _aimPoint - _launcher.position;
-            var lockAngle = Vector3.Angle(_launcher.forward, aimVect);
-            if(lockAngle <= LockSize && Utility.CanSeePoint(_launcher.transform.position, _aimPoint, _target))
-            {
-                Debug.DrawLine(_launcher.position, _aimPoint, Color.green);
-                if(aimVect.magnitude <= FiringRange // in range
-                   && Time.fixedTime - _lockResetTime >= LockTime // locked
-                   && Time.fixedTime - _lastShootTime >= ReloadTime) // loaded
-                {
-                    Fire();
-                }
-            }
-            else
-            {
-                Debug.DrawLine(_launcher.position, _aimPoint, Color.red);
-                _lockResetTime = Time.fixedTime;
-            }
-        }
-        else
-        {
-			_canSeeTarget = false;
+		return aim;
+	}
+    
 
-			if(Time.fixedTime - _lastSeenTime > AttentionSpan)
-            {
-                // pick a new random aim point and wander
-                _aimPoint = _launcher.transform.position + new Vector3(Random.Range(-500, 500), Random.Range(-50, 50), Random.Range(-500, 500));
-                _lastSeenTime = Time.fixedTime;
-            }
-            RotateTowards(_aimPoint);
-        }
-    }
-    
-    void RotateTowards(Vector3 point)
-    {
-        var vect = point - _launcher.position;
-        
-        // rotate platform (y-axis only)
-        var v = new Vector3(vect.x, 0, vect.z);
-        var desiredRotation = Quaternion.LookRotation(v);
-        _platform.rotation = Quaternion.RotateTowards(_platform.rotation, desiredRotation, TurretSpeed * Time.deltaTime);
-        
-        // rotate lancher - (x-axis only)
-        v = new Vector3(0, vect.y, v.magnitude);
-        desiredRotation = Quaternion.LookRotation(v);
-        _launcher.localRotation = Quaternion.RotateTowards(_launcher.localRotation, desiredRotation, TurretSpeed * Time.deltaTime);
-    }
-    
-    void Fire()
-    {
-        _lastShootTime = Time.fixedTime;
-        
-        var pos = _launcher.position;
-        var clone = (GameObject)Instantiate(Projectile, pos, transform.rotation);
-        clone.SendMessage("SetParent", gameObject, SendMessageOptions.DontRequireReceiver);
-        Physics.IgnoreCollision(collider, clone.collider);
-        clone.transform.rotation = _launcher.rotation;
-        clone.rigidbody.AddForce(clone.transform.forward * 100, ForceMode.Impulse);
-    }
-    
-    void ApplyDamage(float amount)
-    {
-        _health -= amount;
-        if(_health <= 0) 
-        {
-            Instantiate(DeathExplosion, transform.position, transform.rotation);
-            Destroy(gameObject);
-        }
-    }
 }
