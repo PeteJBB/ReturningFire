e29d132cd84103bbf21e89057a67f5945bdda64a
PeteJBB <peter.blunden@gmail.com>
2014-06-03 01:34:37 +0100

    Hud targeting basics
    first attempt at HUD targeting - not pretty but functional
    - squares around targets
    - off-screen indicators

--- Assets/My Assets/Scripts/Player/Targeting.cs
+++ Assets/My Assets/Scripts/Player/Targeting.cs
@@ -3,16 +3,62 @@ using System.Collections;
 
 public class Targeting : MonoBehaviour 
 {
+	private Detector _detector;
+	private float _rectSize = 30;
+	private float _animTime = 0.3f;
 
-	// Use this for initialization
-	void Start () 
+	void Start()
 	{
-	
+		_detector = GetComponent<Detector>();
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	void OnGUI () 
 	{
-	
+		var screenRect = new Rect(0,0,Screen.width, Screen.height);
+		foreach(var d in _detector.Detections.Values)
+		{
+			var obj = d.gameObject;
+			if(obj != null)
+			{
+				var vect = obj.transform.position - transform.position;
+				var point = Utility.WorldToGUIPoint(obj.collider.bounds.center);
+
+				// coloring
+				var reverseDetector = obj.GetComponent<Detector>();
+				var color = reverseDetector != null && reverseDetector.CanSee(gameObject)
+					? Color.yellow
+					: Color.white;
+
+				if(screenRect.Contains(point) && point.z > 0)
+				{
+					// get screen rect
+					var t = Mathf.Min((Time.fixedTime - d.firstDetected) / _animTime, 1);
+					var size = Mathf.Lerp(Screen.width, _rectSize, t);
+					var rect = Utility.GetCenteredRectangle(point, size, size);
+
+					// draw rect
+					Drawing.DrawLine(rect.min, new Vector2(rect.xMax, rect.yMin), color, 1, true);
+					Drawing.DrawLine(rect.min, new Vector2(rect.xMin, rect.yMax), color, 1, true);
+					Drawing.DrawLine(rect.max, new Vector2(rect.xMax, rect.yMin), color, 1, true);
+					Drawing.DrawLine(rect.max, new Vector2(rect.xMin, rect.yMax), color, 1, true);
+
+					// range label
+					GUI.TextField(new Rect(rect.xMin, rect.yMax + 2, 1, 1), vect.magnitude.ToString("0"), Utility.BasicGuiStyle);
+				}
+				else
+				{
+					Vector2 edgePoint;
+					if(Utility.RectLineIntersection(screenRect, screenRect.center, point, out edgePoint))
+					{
+						var dir = (edgePoint - screenRect.center).normalized;
+						var p = new Vector2(dir.x * 20, dir.y * 20);
+
+						Drawing.DrawLine(edgePoint - p, edgePoint, color, 5, false);
+					}
+				}
+
+
+			}
+		}
 	}
 }
--- Assets/My Assets/Scripts/Utility.cs
+++ Assets/My Assets/Scripts/Utility.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Utility {
+public class Utility
+{
+	public static Vector3 WorldToGUIPoint(Vector3 position)
+	{
+		var pos = Camera.main.WorldToScreenPoint(position);
+		pos.y = Screen.height - pos.y;
+		if(pos.z < 0)
+			pos *= -1;
+		return pos;
+	}
 
-    public static GUIStyle BasicGuiStyle = new GUIStyle()
+	public static GUIStyle BasicGuiStyle = new GUIStyle()
     {
         clipping = TextClipping.Overflow,
         wordWrap = false,
@@ -28,7 +37,7 @@ public class Utility {
 
 	public static void DrawRotatedGuiTexture(Rect screenRect, float angle, Texture texture, Rect srcRect, Material mat)
 	{
-		if(Event.current.type == EventType.Repaint)
+		if (Event.current.type == EventType.Repaint)
 		{
 			Matrix4x4 matrixBackup = GUI.matrix;
 			GUIUtility.RotateAroundPivot(angle, screenRect.center);
@@ -37,76 +46,76 @@ public class Utility {
 		}
 	}
 
-    public static bool CanSeePoint(Vector3 from, Vector3 to, GameObject target)
-    {
-        RaycastHit hitInfo;
-        bool b = Physics.Linecast(from, to, out hitInfo);
-        if(!b || (target != null && hitInfo.collider.gameObject == target))
-            return true;
-
-        return false;
-    }
-
-	public static bool CanSeeBounds(Vector3 from, Bounds bounds, GameObject target)
+	public static bool CanSeePoint(Vector3 from, Vector3 to, GameObject target)
 	{
 		RaycastHit hitInfo;
-		bool b = Physics.Linecast(from, bounds.center, out hitInfo);
-		if(!b || (target != null && hitInfo.collider.gameObject == target))
-			return true;
-
-		b = Physics.Linecast(from, bounds.max, out hitInfo);
-		if(!b || (target != null && hitInfo.collider.gameObject == target))
-			return true;
-
-		b = Physics.Linecast(from, bounds.min, out hitInfo);
-		if(!b || (target != null && hitInfo.collider.gameObject == target))
+		bool b = Physics.Linecast(from, to, out hitInfo);
+		if (!b || (target != null && hitInfo.collider.gameObject == target))
 			return true;
 
 		return false;
 	}
 
-    public static bool CanObjectSeeAnother(GameObject a, GameObject b)
-    {
-        RaycastHit hitInfo;
-        if (Physics.Linecast(a.transform.position, b.transform.position, out hitInfo)) 
-        {
-            return hitInfo.collider.gameObject == b;
-        }
-        return false;
-    }
+	public static bool CanSeeBounds(Vector3 from, Bounds bounds, GameObject target)
+	{
+		RaycastHit hitInfo;
+		bool b = Physics.Linecast(from, bounds.center, out hitInfo);
+		if (!b || (target != null && hitInfo.collider.gameObject == target))
+			return true;
 
-    public static Rect GetCenteredRectangle(Vector2 center, float width, float height)
-    {
-        var rect = new Rect(center.x - width / 2, center.y - height / 2, width, height);
-        return rect;
-    }
+		b = Physics.Linecast(from, bounds.max, out hitInfo);
+		if (!b || (target != null && hitInfo.collider.gameObject == target))
+			return true;
 
-    public static void AreaDamageEnemies(Vector3 location, float radius, float damage)
-    {
-        var objectsInRange = Physics.OverlapSphere(location, radius);
-        foreach (var col in objectsInRange)
-        {
-            if (col.gameObject != null)
-            {
-                // test if enemy is exposed to blast, or behind cover:
-                //RaycastHit hit;
-                //var exposed = false;
-                //                if (Physics.Raycast (location, (col.transform.position - location), out hit)) 
-                //                {
-                //                    exposed = (hit.collider == col);
-                //                }
-                //                
-                //                if (exposed) 
-                //                {
-                // Damage Enemy! with a linear falloff of damage amount
-                var proximity = (location - col.transform.position).magnitude;
-                var effect = 1 - (proximity / radius);
-                if(effect > 0)
-                    col.gameObject.SendMessage("ApplyDamage", damage * effect, SendMessageOptions.DontRequireReceiver);
-                //}
-            }
-        }
-    }
+		b = Physics.Linecast(from, bounds.min, out hitInfo);
+		if (!b || (target != null && hitInfo.collider.gameObject == target))
+			return true;
+
+		return false;
+	}
+
+	public static bool CanObjectSeeAnother(GameObject a, GameObject b)
+	{
+		RaycastHit hitInfo;
+		if (Physics.Linecast(a.transform.position, b.transform.position, out hitInfo))
+		{
+			return hitInfo.collider.gameObject == b;
+		}
+		return false;
+	}
+
+	public static Rect GetCenteredRectangle(Vector2 center, float width, float height)
+	{
+		var rect = new Rect(center.x - width / 2, center.y - height / 2, width, height);
+		return rect;
+	}
+
+	public static void AreaDamageEnemies(Vector3 location, float radius, float damage)
+	{
+		var objectsInRange = Physics.OverlapSphere(location, radius);
+		foreach (var col in objectsInRange)
+		{
+			if (col.gameObject != null)
+			{
+				// test if enemy is exposed to blast, or behind cover:
+				//RaycastHit hit;
+				//var exposed = false;
+				//                if (Physics.Raycast (location, (col.transform.position - location), out hit)) 
+				//                {
+				//                    exposed = (hit.collider == col);
+				//                }
+				//                
+				//                if (exposed) 
+				//                {
+				// Damage Enemy! with a linear falloff of damage amount
+				var proximity = (location - col.transform.position).magnitude;
+				var effect = 1 - (proximity / radius);
+				if (effect > 0)
+					col.gameObject.SendMessage("ApplyDamage", damage * effect, SendMessageOptions.DontRequireReceiver);
+				//}
+			}
+		}
+	}
 
 	//http://www.morgan-davidson.com/2012/06/19/3d-projectile-trajectory-prediction/
 	Vector3 GetTrajectoryPoint(Vector3 startingPosition, Vector3 initialVelocity, float timestep, Vector3 gravity)
@@ -117,13 +126,13 @@ public class Utility {
 		//Gravity is already in meters per second, so we need meters per second per second
 		Vector3 stepGravity = physicsTimestep * physicsTimestep * gravity;
 		
-		return startingPosition + (timestep * stepVelocity) + ((( timestep * timestep + timestep) * stepGravity ) / 2.0f);
+		return startingPosition + (timestep * stepVelocity) + (((timestep * timestep + timestep) * stepGravity) / 2.0f);
 	}
 
 	public static Vector3 GetTrajectoryVelocity(Vector3 startingPosition, Vector3 targetPosition, float lob, Vector3 gravity)
 	{
 		float physicsTimestep = Time.fixedDeltaTime;
-		float timestepsPerSecond = Mathf.Ceil(1f/physicsTimestep);
+		float timestepsPerSecond = Mathf.Ceil(1f / physicsTimestep);
 		
 		//By default we set n so our projectile will reach our target point in 1 second
 		float n = lob * timestepsPerSecond;
@@ -145,5 +154,140 @@ public class Utility {
 		var angle = (Mathf.Asin(range * gravity.magnitude / (muzzleSpeed * muzzleSpeed))) / 2;
 
 		return angle;
+	}
+
+	public static bool LineIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection)
+	{
+		intersection = Vector2.zero;
+
+		float Ax, Bx, Cx, Ay, By, Cy, d, e, f, num/*,offset*/;
+		float x1lo, x1hi, y1lo, y1hi;
+		Ax = p2.x - p1.x;
+		Bx = p3.x - p4.x;
+		
+		// X bound box test/
+		if (Ax < 0)
+		{
+			x1lo = p2.x;
+			x1hi = p1.x;
+		} 
+		else
+		{
+			x1hi = p2.x;
+			x1lo = p1.x;
+		}
+
+		if (Bx > 0)
+		{
+			if (x1hi < p4.x || p3.x < x1lo)
+				return false;
+		} 
+		else
+		{
+			if (x1hi < p3.x || p4.x < x1lo)
+				return false;
+		}
+
+		Ay = p2.y - p1.y;
+		By = p3.y - p4.y;
+
+		// Y bound box test//
+		if (Ay < 0)
+		{                  
+			y1lo = p2.y;
+			y1hi = p1.y;
+			
+		} 
+		else
+		{
+			y1hi = p2.y;
+			y1lo = p1.y;
+		}
+
+		if (By > 0)
+		{
+			if (y1hi < p4.y || p3.y < y1lo)
+				return false;
+		} 
+		else
+		{
+			if (y1hi < p3.y || p4.y < y1lo)
+				return false;
+		}
+
+		Cx = p1.x - p3.x;
+		Cy = p1.y - p3.y;
+
+		d = By * Cx - Bx * Cy;  // alpha numerator //
+		f = Ay * Bx - Ax * By;  // both denominator //
+
+		// alpha tests //
+		if (f > 0)
+		{
+			if (d < 0 || d > f)
+				return false;
+		} 
+		else
+		{
+			if (d > 0 || d < f)
+				return false;
+		}
+
+		e = Ax * Cy - Ay * Cx;  // beta numerator //
+
+		// beta tests //
+		if (f > 0)
+		{                          
+			if (e < 0 || e > f)
+				return false;
+		} 
+		else
+		{
+			if (e > 0 || e < f)
+				return false;
+		}
+
+		// check if they are parallel
+		if (f == 0)
+			return false;
+		
+		// compute intersection coordinates //
+		num = d * Ax; // numerator //
+		
+		//    offset = same_sign(num,f) ? f*0.5f : -f*0.5f;   // round direction //
+		
+		//    intersection.x = p1.x + (num+offset) / f;
+		intersection.x = p1.x + num / f;
+
+		num = d * Ay;
+		
+		//    offset = same_sign(num,f) ? f*0.5f : -f*0.5f;
+		
+		//    intersection.y = p1.y + (num+offset) / f;
+		intersection.y = p1.y + num / f;
+
+		return true;
+	}
+
+	public static bool RectLineIntersection(Rect r, Vector2 p1, Vector2 p2, out Vector2 intersection)
+	{
+		// top
+		if(LineIntersection(r.min, new Vector2(r.xMax, r.yMin), p1, p2, out intersection))
+			return true;
+
+		// left
+		if(LineIntersection(r.min, new Vector2(r.xMin, r.yMax), p1, p2, out intersection))
+			return true;
+
+		// bottom
+		if(LineIntersection(r.max, new Vector2(r.xMin, r.yMax), p1, p2, out intersection))
+			return true;
+
+		// right
+		if(LineIntersection(r.max, new Vector2(r.xMax, r.yMin), p1, p2, out intersection))
+			return true;
+
+		intersection = Vector2.zero;
+		return false;
 	}
 }
